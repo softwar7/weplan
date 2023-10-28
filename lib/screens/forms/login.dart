222cf95b9031dcd341f46a86de28ac81a908f3f3
parkjbdev <dev@parkjb.com>
2023-10-28 16:17:01 +0900

    feat: add login form & add validators for sign forms

---
+++ lib/screens/forms/login.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+import 'package:weplan/services/validator.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: LoginScaffold(),
+    ),
+  );
+}
+
+class LoginScaffold extends StatefulWidget {
+  const LoginScaffold({super.key});
+
+  @override
+  State<LoginScaffold> createState() => _LoginScaffoldState();
+}
+
+class _LoginScaffoldState extends State<LoginScaffold> {
+  final _formKey = GlobalKey<FormState>();
+
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+  bool _passwordVisible = false;
+
+  String? loginId = '';
+  String? password = '';
+
+  String? validate(String? value, bool Function(String) validator) {
+    try {
+      if (value == null) throw '해당 필드를 입력해주세요. ';
+      if (validator(value)) return null;
+      throw 'Unknown error';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        child: Form(
+          key: _formKey,
+          autovalidateMode: _autovalidateMode,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                autofillHints: const [AutofillHints.username],
+                validator: (value) => validate(value, Validator.loginId),
+                onSaved: (value) => loginId = value!,
+                decoration: const InputDecoration(
+                  labelText: '아이디',
+                ),
+              ),
+              TextFormField(
+                obscureText: !_passwordVisible,
+                autofillHints: const [AutofillHints.password],
+                validator: (value) => validate(value, Validator.password),
+                onSaved: (value) => password = value!,
+                decoration: InputDecoration(
+                  labelText: '비밀번호',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _passwordVisible
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    // TODO: Login
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          {
+                            'loginId': loginId!,
+                            'password': password!,
+                          }.toString(),
+                        ),
+                      ),
+                    );
+                  } else {
+                    setState(() {
+                      _autovalidateMode = AutovalidateMode.onUserInteraction;
+                    });
+                  }
+                },
+                child: const Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/validator.dart
@@ -0,0 +1,73 @@
+import 'package:weplan/models/enum/role_type.dart';
+
+class Validator {
+  static bool loginId(String value) {
+    // min 6 max 15, small case, starts with letter
+    if (RegExp(r'^[a-z][a-z0-9]{5,14}$').hasMatch(value)) {
+      return true;
+    } else {
+      throw '아이디는 6~15 자리여야 합니다.';
+    }
+  }
+
+  static bool name(String value) {
+    // min 1 max 11, don't start or end with whitespace
+    if (RegExp(r'^[^\s].{0,9}[^\s]$').hasMatch(value)) {
+      return true;
+    } else {
+      throw '이름은 1~11 자리이며, 공백으로 시작하거나 끝나지 않아야 합니다.';
+    }
+  }
+
+  static bool phoneNumber(String value) {
+    // only numbers, 11 digits
+    if (RegExp(r'^[0-9]{10,11}$').hasMatch(value)) {
+      return true;
+    } else {
+      throw '전화번호는 10~11 자리의 -을 제외한 숫자여야 합니다.';
+    }
+  }
+
+  static bool roleType(String value) {
+    if (RoleType.values.map((e) => e.toString()).contains(value)) {
+      return true;
+    } else {
+      throw '권한은 ADMIN 또는 GUEST 여야 합니다.';
+    }
+  }
+
+  static bool adminPassword(String value) {
+    // min 8 max 15
+    if (RegExp(r'^.{8,15}$').hasMatch(value)) {
+      return true;
+    } else {
+      throw '관리자 인증번호는 8~15 자리여야 합니다.';
+    }
+  }
+
+  static bool password(String value) {
+    // min 8 max 15 should include number, capital letter, small letter, special character
+    if (RegExp(
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,15}$',
+    ).hasMatch(value)) {
+      return true;
+    } else {
+      throw '비밀번호는 8~15 자리의 숫자, 대문자, 소문자, 특수문자를 포함해야 합니다.';
+    }
+  }
+
+  String _password = '';
+
+  bool newPassword(String value) {
+    _password = value;
+    return password(value);
+  }
+
+  bool newPasswordCheck(String value) {
+    if (value == _password) {
+      return true;
+    } else {
+      throw '비밀번호가 일치하지 않습니다.';
+    }
+  }
+}
