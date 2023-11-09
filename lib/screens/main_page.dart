c69d7afcbcfbe164f452094c1f1fb52554a050f0
parkjbdev <dev@parkjb.com>
2023-11-09 18:18:13 +0900

    refactor: navigationdrawer selectIndex logic

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -8,9 +8,19 @@ import 'package:weplan/components/drawer/divider.dart';
 import 'package:weplan/components/drawer/subject.dart';
 import 'package:weplan/components/snackbar.dart';
 import 'package:weplan/components/timetable.dart';
-import 'package:weplan/models/channel.dart';
+import 'package:weplan/screens/forms/channel_form.dart';
 import 'package:weplan/services/api_service.dart';
 import 'package:weplan/services/auth_service.dart';
+
+class Menu {
+  NavigationDrawerDestination navDrawer;
+  Widget body;
+
+  Menu({
+    required this.navDrawer,
+    required this.body,
+  });
+}
 
 var items = [
   TimetableItem(
@@ -53,7 +63,41 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  List<Channel> channels = [];
+  List<Menu> channels = [];
+  List<Menu> admins = [
+    Menu(
+      navDrawer: const NavigationDrawerDestination(
+        icon: Icon(Icons.add_rounded),
+        label: Text('채널 생성'),
+      ),
+      body: ChannelForm(),
+      // body: Container(),
+    ),
+    Menu(
+      navDrawer: const NavigationDrawerDestination(
+        icon: Icon(Icons.task_outlined),
+        label: Text('예약요청 목록'),
+      ),
+      body: Container(),
+    ),
+  ];
+  List<Menu> etc = [
+    Menu(
+      navDrawer: const NavigationDrawerDestination(
+        icon: Icon(Icons.list_outlined),
+        label: Text('나의 예약'),
+      ),
+      body: Container(child: const Text('my reservation')),
+    ),
+    Menu(
+      navDrawer: const NavigationDrawerDestination(
+        icon: Icon(Icons.logout),
+        label: Text('로그아웃'),
+      ),
+      body: Container(child: const Text('logout')),
+    ),
+  ];
+
   int _selectedIndex = 0;
 
   void handleSelect(int index) {
@@ -61,15 +105,9 @@ class _MainPageState extends State<MainPage> {
       _selectedIndex = index;
     });
 
-    int myReservationIndex =
-        channels.length + (context.read<AuthService>().isAdmin ? 2 : 0);
-    if (index == myReservationIndex) {
-      // TODO: 나의 예약 페이지로 이동
-      showSnackBar(context, '나의 예약');
-      return;
-    }
+    int myReservationIndex = channels.length + admins.length + 0;
+    int logoutIndex = channels.length + admins.length + 1;
 
-    int logoutIndex = myReservationIndex + 1;
     if (index == logoutIndex) {
       handleLogout();
       return;
@@ -96,12 +134,25 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  @override
-  void initState() {
-    super.initState();
+  void updateChannels() {
     context.read<ApiService>().getChannels().then((value) {
       setState(() {
-        channels = value;
+        channels = value
+            .map(
+              (e) => Menu(
+                navDrawer: NavigationDrawerDestination(
+                  icon: const Icon(Icons.calendar_month_outlined),
+                  label: Text(e.name),
+                ),
+                body: TimeTableComponent(
+                  items: items,
+                  onTap: (item) {
+                    showSnackBar(context, item.toString());
+                  },
+                ),
+              ),
+            )
+            .toList();
       });
       showSnackBar(context, '채널목록을 불러왔습니다.');
     }).catchError((e) {
@@ -111,6 +162,17 @@ class _MainPageState extends State<MainPage> {
         showErrorSnackBar(context, '채널목록을 불러오는 중 오류가 발생했습니다.');
       throw e;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (!context.read<AuthService>().isAdmin)
+      setState(() {
+        admins = [];
+      });
+    // Update Channels
+    updateChannels();
   }
 
   @override
@@ -127,46 +189,30 @@ class _MainPageState extends State<MainPage> {
         children: [
           // Channel Menu
           if (channels.isNotEmpty) const DrawerSubjects('채널목록'),
-          ...channels.map(
-            (e) => NavigationDrawerDestination(
-              icon: const Icon(Icons.calendar_month_outlined),
-              label: Text(e.name),
-            ),
-          ),
+          ...channels.map((channel) => channel.navDrawer),
           if (channels.isNotEmpty) const DrawerDivider(),
 
           // Admin Menu
-          if (authService.isAdmin) const DrawerSubjects('관리자'),
-          if (authService.isAdmin)
-            const NavigationDrawerDestination(
-              icon: Icon(Icons.add_rounded),
-              label: Text('채널 생성'),
-            ),
-          if (authService.isAdmin)
-            const NavigationDrawerDestination(
-              icon: Icon(Icons.task_outlined),
-              label: Text('예약요청 목록'),
-            ),
-          if (authService.isAdmin) const DrawerDivider(),
+          if (admins.isNotEmpty) const DrawerSubjects('관리자'),
+          if (admins.isNotEmpty) ...admins.map((admin) => admin.navDrawer),
+          if (admins.isNotEmpty) const DrawerDivider(),
 
-          const NavigationDrawerDestination(
-            icon: Icon(Icons.list_outlined),
-            label: Text('나의 예약'),
-          ),
-          const NavigationDrawerDestination(
-            icon: Icon(Icons.logout),
-            label: Text('로그아웃'),
-          ),
+          ...etc.map((e) => e.navDrawer),
         ],
       ),
+      body: [...channels, ...admins, ...etc][_selectedIndex].body,
+      // body: channels.isEmpty
+      //     ? const Center(child: CircularProgressIndicator())
+      //     : channels[_selectedIndex].body,
       // TODO: 매칭되는 페이지 표시
+      // body: menus[_selectedIndex].body,
       // body: TimeTable(),
-      body: TimeTableComponent(
-        items: items,
-        onTap: (item) {
-          showSnackBar(context, item.toString());
-        },
-      ),
+      // body: TimeTableComponent(
+      //   items: items,
+      //   onTap: (item) {
+      //     showSnackBar(context, item.toString());
+      //   },
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Add',
