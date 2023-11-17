949b1470f21e5b77448676219a13e16c59d3029d
parkjbdev <dev@parkjb.com>
2023-11-17 23:24:33 +0900

    refactor: accept TimeTableComponent to change startHour & endHour & type cast `item.data` as ScheduleViewModel

--- lib/components/timetable.dart
+++ lib/components/timetable.dart
@@ -2,25 +2,32 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_timetable/flutter_timetable.dart';
 
+import 'package:weplan/viewmodels/schedule.dart';
+
 class TimeTableComponent extends StatelessWidget {
   final List<TimetableItem> items;
   final void Function(TimetableItem) onTap;
+  final int startHour;
+  final int endHour;
+  final TimetableController controller;
+
   TimeTableComponent({
     super.key,
     required this.items,
     required this.onTap,
-  });
-
-  final controller = TimetableController(
-    timelineWidth: 70,
-    initialColumns: 7,
-    cellHeight: 60,
-    startHour: 9,
-    endHour: 21,
-    start: DateTime.now().subtract(
-      Duration(days: DateTime.now().weekday - 1),
-    ), // Start on Monday
-  );
+    this.startHour = 9,
+    this.endHour = 21,
+  })  : assert(startHour < endHour),
+        controller = TimetableController(
+          timelineWidth: 70,
+          initialColumns: 7,
+          cellHeight: 60,
+          startHour: startHour,
+          endHour: endHour,
+          start: DateTime.now().subtract(
+            Duration(days: DateTime.now().weekday - 1),
+          ), // Start on Monday
+        );
 
   int colorTime(DateTime time) {
     DateTime now = DateTime.now();
@@ -52,22 +59,21 @@ class TimeTableComponent extends StatelessWidget {
       },
       hourLabelBuilder: (time) => Text('${time.hour}시'),
       items: items,
-      itemBuilder: (item) => GestureDetector(
-        onTap: () {
-          this.onTap(item);
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            color: Color(colorTime(item.start)),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Center(
-            child: Text(
-              item.data ?? '',
+      itemBuilder: (item) {
+        ScheduleViewModel schedule = item.data as ScheduleViewModel;
+        return GestureDetector(
+          onTap: () => this.onTap(item),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(colorTime(item.start)),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Text(schedule.name),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
